d0d998843fa8f203b6b5f0dd1698855565788a59
Jeff Walker <walker.jeff.d@gmail.com>
2017-10-16 15:43:06 -0400

    update api /predictions to new format and with status objects

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
+const moment = require('moment-timezone');
 
 const config = require('../config');
 const db = require('./db');
@@ -45,17 +46,84 @@ app.get('/', (req, res) => {
   res.status(200).json({ status: 'ok', data: [] });
 });
 
+function assignStatus(prediction) {
+  let type = 'unknown';
+  let label = 'Not Available';
+  let reason = 'Data unavailable or system error';
+
+  if (_.isNull(prediction.exceedance) || _.isUndefined(prediction.exceedance)) {
+    type = 'unknown';
+    label = 'Not Available';
+    reason = 'Data unavailable or system error.';
+  } else if (prediction.exceedance) {
+    type = 'advisory';
+    label = 'Advisory';
+    reason = 'High probability of elevated bacteria levels.';
+  } else {
+    type = 'good';
+    label = 'Good';
+    reason = 'Low probability of elevated bacteria levels.';
+  }
+
+  return { type, label, reason };
+}
+
+const sites = {
+  MYSTIC_ECOLI: {
+    name: 'Mystic River',
+    description: 'Mystic Valley Parkway (Rt 16)',
+    latitude: 42.405722,
+    longitude: -71.096351
+  },
+  MALDENLOWER_ECOLI: {
+    name: 'Malden River',
+    description: 'Revere Beach Parkway (Rt 16)',
+    latitude: 42.4053,
+    longitude: -71.07191
+  },
+  SHANNON_ENT: {
+    name: 'Upper Mystic Lake',
+    description: 'Shannon Beach',
+    latitude: 42.439892,
+    longitude: -71.146153
+  }
+};
+
 app.get('/predictions/', (req, res, next) => {
   db.getPredictions()
-    .then((result) => {
-      const names = _.uniq(result.map(d => d.name));
+    .then((results) => {
+      // randomize exceedances
+      results.forEach((d) => {
+        d.prob = Math.random();
+        d.exceedance = d.prob > 0.45;
+        if (d.prob > 0.9) {
+          d.prob = null;
+          d.exceedance = null;
+        }
+      });
 
+      // add local timestamp string
+      results.forEach((d) => {
+        d.timestamp_local = moment.tz(d.timestamp, 'US/Eastern').format('MMM D YYYY H:mm a z');
+      });
+
+      // create response data
+      const names = _.uniq(results.map(d => d.name));
       const data = names.map((name) => {
-        return {
+        const d = {
           name,
-          current: result.filter(d => d.name === name)[0],
-          history: result.filter(d => d.name === name)
+          site: sites[name],
+          current: results.filter(p => p.name === name)[0],
+          history: results.filter(p => p.name === name),
+          flags: []
         };
+
+        // assign status
+        d.current.status = assignStatus(d.current, d.flags);
+        d.history.forEach((p) => {
+          p.status = assignStatus(p, d.flags);
+        });
+        return d;
       });
 
       return data;
