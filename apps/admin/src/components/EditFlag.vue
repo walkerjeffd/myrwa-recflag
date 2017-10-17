d1ab72dafa6026632a7914502071362e04cbc876
Jeff Walker <walker.jeff.d@gmail.com>
2017-10-17 16:29:18 -0400

    update edit and new flag forms in admin app

--- apps/admin/src/components/EditFlag.vue
+++ apps/admin/src/components/EditFlag.vue
@@ -5,29 +5,53 @@
     <b-row v-if="!loading">
       <b-col cols="6">
         <b-form @submit="onSubmit">
-          <b-form-group id="labelSite" label="Site:" label-for="inputSite">
-            <b-form-select id="inputSite" :options="sites" required v-model="form.site"></b-form-select>
-          </b-form-group>
-          <b-form-group id="labelStart" label="Start Date/Time:" label-for="inputStartDate">
+          <b-form-group id="labelLocation" label="Location:" label-for="inputLocation">
             <b-form-row>
-              <b-col><b-form-input id="inputStartDate" type="date" required v-model="form.start.date"></b-form-input></b-col>
-              <b-col><b-form-input id="inputStartTime" type="time" required v-model="form.start.time"></b-form-input></b-col>
+              <b-col cols="6"><b-form-select id="inputLocation" :options="locations" required v-model="form.location_id"></b-form-select></b-col>
             </b-form-row>
           </b-form-group>
-          <b-form-group id="labelEnd" label="End Date/Time:" label-for="inputEndDate">
-            <b-form-row>
-              <b-col><b-form-input id="inputEndDate" type="date" required v-model="form.end.date"></b-form-input></b-col>
-              <b-col><b-form-input id="inputEndTime" type="time" required v-model="form.end.time"></b-form-input></b-col>
-            </b-form-row>
-          </b-form-group>
-          <b-form-group id="labelType" label="Flag Type:" label-for="inputType">
-            <b-form-select id="inputType" :options="types" required v-model="form.type"></b-form-select>
-          </b-form-group>
+          <b-form-row>
+            <b-col>
+              <b-form-group id="labelStartDate" label="Start Date:" label-for="inputStartDate">
+                <b-form-input id="inputStartDate" type="date" required v-model="form.start.date"></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group id="labelStartTime" label="Start Time:" label-for="inputStartTime">
+                <b-form-input id="inputStartTime" type="time" required v-model="form.start.time"></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-form-row>
+          <b-form-row>
+            <b-col>
+              <b-form-group id="labelEndDate" label="End Date:" label-for="inputEndDate">
+                <b-form-input id="inputEndDate" type="date" required v-model="form.end.date"></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group id="labelEndTime" label="End Time:" label-for="inputEndTime">
+                <b-form-input id="inputEndTime" type="time" required v-model="form.end.time"></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-form-row>
+          <b-form-row>
+            <b-col>
+              <b-form-group id="labelType" label="Flag Type:" label-for="inputType">
+                <b-form-select id="inputType" :options="types" required v-model="form.type"></b-form-select>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group id="labelLevel" label="Flag Level:" label-for="inputLevel">
+                <b-form-select id="inputLevel" :options="levels" required v-model="form.level"></b-form-select>
+              </b-form-group>
+            </b-col>
+          </b-form-row>
           <b-form-group id="labelDescription" label="Description:" label-for="inputDescription">
-            <b-form-input id="inputDescription" type="text" required v-model="form.description"></b-form-input>
+            <b-form-input id="inputDescription" type="text" placeholder="Custom message that will appear on website" required v-model="form.description"></b-form-input>
           </b-form-group>
           <b-button type="submit" variant="primary">Submit</b-button>
-          <b-button to="/">Cancel</b-button>
+          <b-button type="reset" variant="secondary">Reset</b-button>
+          <b-button variant="danger" to="/">Cancel</b-button>
         </b-form>
       </b-col>
     </b-row>
@@ -41,7 +65,7 @@ export default {
     return {
       loading: true,
       form: {
-        site: null,
+        location_id: null,
         start: {
           date: '',
           time: ''
@@ -51,16 +75,19 @@ export default {
           time: ''
         },
         type: null,
+        level: null,
         description: ''
       },
       types: [
-        { value: 'CYANO_UNCERTAIN', text: 'Unconfirmed Cyanobacteria (Uncertain)'},
-        { value: 'CYANO_ADVISORY', text: 'Confirmed Cyanobacteria (Advisory)'},
-        { value: 'CSO_ADVISORY', text: 'Combined Sewer Overflow (Advisory)'},
-        { value: 'OTHER_ADVISORY', text: 'Other (Advisory)'},
-        { value: 'OTHER_UNCERTAIN', text: 'Other (Uncertain)'}
+        { value: 'CYANO', text: 'Cyanobacteria'},
+        { value: 'CSO', text: 'Combined Sewer Overflow'},
+        { value: 'OTHER', text: 'Other'}
       ],
-      sites: [
+      levels: [
+        { value: 'UNCERTAIN', text: 'Uncertain'},
+        { value: 'ADVISORY', text: 'Advisory'}
+      ],
+      locations: [
         { value: 'MYSTIC_ECOLI', text: 'Mystic River (Rt 16)' },
         { value: 'MALDENLOWER_ECOLI', text: 'Malden River (Rt 16)' },
         { value: 'SHANNON_ENT', text: 'Upper Mystic Lake (Shannon Beach)' }
@@ -79,9 +106,10 @@ export default {
         date: '2017-10-17',
         time: '18:25'
       },
-      site: 'MYSTIC_ECOLI',
-      type: 'CYANO_ADVISORY',
-      description: 'Cyanobacteria'
+      location_id: 'MYSTIC_ECOLI',
+      type: 'CYANO',
+      level: 'ADVISORY',
+      description: 'Cyanobacteria bloom!'
     };
     setTimeout(() => {
       this.form = flag;
--- apps/admin/src/components/NewFlag.vue
+++ apps/admin/src/components/NewFlag.vue
@@ -4,26 +4,49 @@
     <b-row>
       <b-col cols="6">
         <b-form @submit="onSubmit">
-          <b-form-group id="labelSite" label="Site:" label-for="inputSite">
-            <b-form-select id="inputSite" :options="sites" required v-model="form.site"></b-form-select>
-          </b-form-group>
-          <b-form-group id="labelStart" label="Start Date/Time:" label-for="inputStartDate">
+          <b-form-group id="labelLocation" label="Location:" label-for="inputLocation">
             <b-form-row>
-              <b-col><b-form-input id="inputStartDate" type="date" required v-model="form.start.date"></b-form-input></b-col>
-              <b-col><b-form-input id="inputStartTime" type="time" required v-model="form.start.time"></b-form-input></b-col>
+              <b-col cols="6"><b-form-select id="inputLocation" :options="locations" required v-model="form.location_id"></b-form-select></b-col>
             </b-form-row>
           </b-form-group>
-          <b-form-group id="labelEnd" label="End Date/Time:" label-for="inputEndDate">
-            <b-form-row>
-              <b-col><b-form-input id="inputEndDate" type="date" required v-model="form.end.date"></b-form-input></b-col>
-              <b-col><b-form-input id="inputEndTime" type="time" required v-model="form.end.time"></b-form-input></b-col>
-            </b-form-row>
-          </b-form-group>
-          <b-form-group id="labelType" label="Flag Type:" label-for="inputType">
-            <b-form-select id="inputType" :options="types" required v-model="form.type"></b-form-select>
-          </b-form-group>
+          <b-form-row>
+            <b-col>
+              <b-form-group id="labelStartDate" label="Start Date:" label-for="inputStartDate">
+                <b-form-input id="inputStartDate" type="date" required v-model="form.start.date"></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group id="labelStartTime" label="Start Time:" label-for="inputStartTime">
+                <b-form-input id="inputStartTime" type="time" required v-model="form.start.time"></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-form-row>
+          <b-form-row>
+            <b-col>
+              <b-form-group id="labelEndDate" label="End Date:" label-for="inputEndDate">
+                <b-form-input id="inputEndDate" type="date" required v-model="form.end.date"></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group id="labelEndTime" label="End Time:" label-for="inputEndTime">
+                <b-form-input id="inputEndTime" type="time" required v-model="form.end.time"></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-form-row>
+          <b-form-row>
+            <b-col>
+              <b-form-group id="labelType" label="Flag Type:" label-for="inputType">
+                <b-form-select id="inputType" :options="types" required v-model="form.type"></b-form-select>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group id="labelLevel" label="Flag Level:" label-for="inputLevel">
+                <b-form-select id="inputLevel" :options="levels" required v-model="form.level"></b-form-select>
+              </b-form-group>
+            </b-col>
+          </b-form-row>
           <b-form-group id="labelDescription" label="Description:" label-for="inputDescription">
-            <b-form-input id="inputDescription" type="text" required v-model="form.description"></b-form-input>
+            <b-form-input id="inputDescription" type="text" placeholder="Custom message that will appear on website" required v-model="form.description"></b-form-input>
           </b-form-group>
           <b-button type="submit" variant="primary">Submit</b-button>
           <b-button type="reset" variant="secondary">Reset</b-button>
@@ -53,7 +76,7 @@ export default {
       //   description: ''
       // },
       form: {
-        site: 'MYSTIC_ECOLI',
+        location_id: 'MYSTIC_ECOLI',
         start: {
           date: '2017-10-15',
           time: '00:00'
@@ -62,17 +85,20 @@ export default {
           date: '2017-10-17',
           time: '12:00'
         },
-        type: 'CYANO_UNCERTAIN',
-        description: 'Unconfirmed cyanobacteria'
+        type: 'CYANO',
+        level: 'ADVISORY',
+        description: 'Confirmed cyanobacteria bloom'
       },
       types: [
-        { value: 'CYANO_UNCERTAIN', text: 'Unconfirmed Cyanobacteria (Uncertain)'},
-        { value: 'CYANO_ADVISORY', text: 'Confirmed Cyanobacteria (Advisory)'},
-        { value: 'CSO_ADVISORY', text: 'Combined Sewer Overflow (Advisory)'},
-        { value: 'OTHER_ADVISORY', text: 'Other (Advisory)'},
-        { value: 'OTHER_UNCERTAIN', text: 'Other (Uncertain)'}
+        { value: 'CYANO', text: 'Cyanobacteria'},
+        { value: 'CSO', text: 'Combined Sewer Overflow'},
+        { value: 'OTHER', text: 'Other'}
       ],
-      sites: [
+      levels: [
+        { value: 'UNCERTAIN', text: 'Uncertain'},
+        { value: 'ADVISORY', text: 'Advisory'}
+      ],
+      locations: [
         { value: 'MYSTIC_ECOLI', text: 'Mystic River (Rt 16)' },
         { value: 'MALDENLOWER_ECOLI', text: 'Malden River (Rt 16)' },
         { value: 'SHANNON_ENT', text: 'Upper Mystic Lake (Shannon Beach)' }
