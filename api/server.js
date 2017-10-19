5bb5e800219f96705fc76c3dfc91fea2f9b33cfa
Jeff Walker <walker.jeff.d@gmail.com>
2017-10-19 14:08:40 -0400

    add OPTIONS to cross domain api

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 // allow CORS
 const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   next();
 };
